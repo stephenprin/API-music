9ad9a21936022cc478cb43bddea5aecb9be90ba2
stephenprin <stephenprince427@gmail.com>
2023-01-20 23:36:38 +0100

    more crud operation added on the artist controller

--- src/controller/artistController.ts
+++ src/controller/artistController.ts
@@ -20,3 +20,98 @@ export const artist = async (req: Request, res: Response, next: NextFunction) =>
     }
 }
 
+export const getArtist = async (req: Request, res: Response, next: NextFunction) => { 
+    try {
+        const artist = await Artist.findOne({ _id: req.params.id });
+        if (artist) {
+            return res.status(200).json({
+                message: 'Artist fetched successfully',
+                artist
+            });
+        } else {
+            return res.status(404).json({
+                message: 'Artist not found'
+            });
+        }
+    } catch (err) {
+        return res.status(400).json({
+            message: err
+        });
+    }
+
+}
+
+export const getAllArtist = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const options = {
+            sort: {
+                createdAt: 1
+            }
+        }
+        const artist = await Artist.find(options);
+        if (artist) { 
+            return res.status(200).json({
+                message: 'Artist fetched successfully',
+                artist
+            });
+        } else {
+            return res.status(404).json({
+                message: 'Artist not found'
+            });
+        }
+    } catch (error) {
+        res.status(400).json({ 
+            message: error
+        });
+    }
+}
+ 
+
+export const updateArtist = async (req: Request, res: Response, next: NextFunction) => { 
+    try {
+        
+        const result = await Artist.findOneAndUpdate(
+            { _id: req.params.id },
+            req.body,
+            { new: true },
+    
+             
+            
+        );
+
+        if (result) { 
+            return res.status(200).json({
+                message: 'Artist updated successfully',
+                result
+            });
+        } else {
+            return res.status(404).json({
+                message: 'Artist not found'
+            });
+        }
+    } catch (error) {
+        res.status(400).json({
+            message: error
+        });
+    }
+}
+
+export const deleteArtist = async (req: Request, res: Response, next: NextFunction) => { 
+    try {
+        const result = await Artist.findOneAndDelete({ _id: req.params.id });
+        if (result) { 
+            return res.status(200).json({
+                message: 'Artist deleted successfully',
+                result
+            });
+        } else {
+            return res.status(404).json({
+                message: 'Artist not found'
+            });
+        }
+    } catch (error) {
+        res.status(400).json({
+            message: error
+        });
+    }
+}
--- src/routes/artistRoute.ts
+++ src/routes/artistRoute.ts
@@ -1,9 +1,13 @@
 import express from 'express';
-import { artist } from '../controller/artistController';
+import { artist , getArtist, getAllArtist, deleteArtist,updateArtist} from '../controller/artistController';
 
 const router = express.Router();
 
 router.post('/create-artist', artist)
+router.get('/get-one/:id', getArtist)
+router.get('/get-all', getAllArtist)
+router.delete('/delete/:id', deleteArtist)
+router.patch('/update/:id', updateArtist)
 
 
 export default router;
